9ce9b84c6ba01a6af93af612ee1a3a4d4217bcd7
Samuel Chigozie <Nzekwesammy@gmail.com>
2023-04-26 19:38:43 -0700

    Update builtin_cmds.c

--- builtin_cmds.c
+++ builtin_cmds.c
@@ -4,7 +4,7 @@ int status;
 
 /**
  * _setenv - sets and environmental variable
- * @name: name of the variable
+ * @name: name of the variable..
  * @value: value to set the variable to
  *
  * Return: 0 on success
